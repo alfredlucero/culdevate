374b313c0740edb0bb3d04a0e601f4b8b0570ca0
alfredlucero <alfred.lucero@ucla.edu>
2019-04-15 07:42:25 -0700

    Added Next, TS, Storybook, Babel

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const CuldevateApp = () => (
+  <div>
+    <Title>Culdevate App</Title>
+  </div>
+);
+
+const Title = styled.h1`
+  color: cyan;
+`;
+
+export default CuldevateApp;

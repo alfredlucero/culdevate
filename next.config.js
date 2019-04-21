7b025fbe4994e9a05efc61cf397f51c1c16e5a41
alfredlucero <alfred.lucero@ucla.edu>
2019-04-21 16:58:57 -0700

    Integrating Now for CICD

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTypescript = require("@zeit/next-typescript");
 
-module.exports = withTypescript();
+module.exports = withTypescript({
+  // Each page will be built as an individual serverless lambda
+  target: "serverless",
+});

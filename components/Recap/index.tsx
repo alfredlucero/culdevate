83229e0e4b72726de95e770da2475e8ded9d945e
alfredlucero <alfred.lucero@ucla.edu>
2019-04-25 01:05:41 -0700

    Added Nunito/Lato fonts and default styles to storybook preview-head, mapped recap type to icon in recap component

--- components/Recap/index.tsx
+++ components/Recap/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Icon, { CuldevateIcon } from "../__common__/Icon";
 
 export type RecapType =
   | "objectives"
@@ -19,10 +20,27 @@ export interface RecapProps {
   endDate: string | null;
 }
 
+const mapRecapTypeToIcon = (recapType: RecapType): React.ReactNode | null => {
+  const recapTypeToIconMap: { [key in RecapType]: CuldevateIcon } = {
+    objectives: "target",
+    work: "suitcase",
+    education: "school",
+    extracurricular: "userFriends",
+    skills: "levelUp",
+    achievements: "trophy",
+    awards: "award",
+    proof: "pen",
+    other: "checkDouble",
+  };
+  const mappedIcon = recapTypeToIconMap[recapType];
+
+  return mappedIcon ? <Icon icon={mappedIcon} /> : null;
+};
+
 const Recap: React.FC<RecapProps> = ({ title, description, type, startDate, endDate }) => {
   return (
     <div>
-      {type}
+      {mapRecapTypeToIcon(type)}
       <h2>{title}</h2>
       <p>{description}</p>
       <p data-testid="recapDateRange">

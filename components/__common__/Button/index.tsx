7dc05e4b8ff2c7b4e7e0a99e7dfca7d69d151d11
alfredlucero <alfred.lucero@ucla.edu>
2019-05-08 21:39:30 -0700

    Sketched out props for Button component

--- components/__common__/Button/index.tsx
+++ components/__common__/Button/index.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+import { culdevateDefaultTheme } from "../../defaultTheme";
+
+interface StyledButtonProps {
+  onClick: (event: React.MouseEvent) => void;
+  disabled: boolean;
+  kind: ButtonKind;
+  type: ButtonType;
+  size: ButtonSize;
+}
+const StyledButton = styled.button<StyledButtonProps>`
+  border-radius: 5px;
+`;
+StyledButton.defaultProps = {
+  theme: culdevateDefaultTheme,
+};
+
+export type ButtonKind = "primary" | "danger" | "secondary-primary" | "secondary-danger";
+export type ButtonType = "button" | "submit" | "reset";
+export type ButtonSize = "small" | "medium" | "large";
 
 interface ButtonProps {
-  testId: string;
+  onClick: (event: React.MouseEvent) => void;
+  children: React.ReactNode;
+  kind: ButtonKind;
+  type: ButtonType;
+  size: ButtonSize;
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ testId }) => {
-  return <button data-testid={testId}>Button</button>;
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  children,
+  kind,
+  type,
+  size,
+  disabled = false,
+  ...passThroughProps
+}) => {
+  return (
+    <StyledButton onClick={onClick} kind={kind} type={type} size={size} disabled={disabled} {...passThroughProps}>
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;

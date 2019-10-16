86233019b17fbd6f2d3b3dfd40a840f41d18efe9
alfredlucero <alfred.lucero@ucla.edu>
2019-10-15 22:02:10 -0700

    [#22] Set up frontend repo with React, TS, and Webpack

---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+# Culdevate Frontend
+
+## How to Get Started
+
+Install all dependencies with `npm ci`.
+
+Start up the Webpack dev server by doing `npm run start`. Go to `http://localhost:8080` to
+see your code changes.
+
+## Building Production Assets
+
+Build out the production assets by doing `npm run build` and see the outputs in the `dist` folder.
---
+++ frontend/webpack.config.js
@@ -0,0 +1,24 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/Main.tsx",
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["dist"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/templates/index.html",
+    }),
+  ],
+  output: {
+    path: __dirname + "/dist",
+    filename: "dist/[name].[contenthash].js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+  },
+};

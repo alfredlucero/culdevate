fd36af2530217c20cc80d1864f0979c5231cb557
alfredlucero <alfred.lucero@ucla.edu>
2019-10-20 14:46:00 -0700

    [#26] Added Jest and React Testing Library for unit tests

---
+++ frontend/src/@types/jest-dom.d.ts
@@ -0,0 +1,3 @@
+// This allows us to not import in every unit test file to use the extended Jest assertions
+// For all extended assertions see: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect";
--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>
+    <div data-testid="landingPage">
       <h2>Culdevate Landing</h2>
     </div>
   );

bb758de7db0090a6800211a3b686e5d25af70e91
alfredlucero <alfred.lucero@ucla.edu>
2020-01-27 21:37:25 -0800

    [#72] Added close svg icon and implemented alert unit tests/stories

--- packages/frontend/src/components/Alert/index.test.tsx
+++ packages/frontend/src/components/Alert/index.test.tsx
@@ -6,6 +6,46 @@ describe("<Alert />", () => {
   const alertTestId = "alertTestId";
 
   test("should render without error", () => {
-    expect(true).toBe(true);
+    const { container } = render(
+      <div>
+        <Alert variant="success" onClose={() => {}} isVisible={true} testId={alertTestId} className="extra-alert-class">
+          Success Alert
+        </Alert>
+        <Alert variant="warning" onClose={() => {}} isVisible={true} testId={alertTestId} className="extra-alert-class">
+          Warning Alert
+        </Alert>
+        <Alert variant="info" onClose={() => {}} isVisible={true} testId={alertTestId} className="extra-alert-class">
+          Info Alert
+        </Alert>
+        <Alert variant="danger" onClose={() => {}} isVisible={true} testId={alertTestId} className="extra-alert-class">
+          Danger Alert
+        </Alert>
+      </div>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should not exist when the alert is not visible", () => {
+    const { queryByTestId } = render(
+      <Alert variant="success" onClose={() => {}} isVisible={false} testId={alertTestId}>
+        Success Alert
+      </Alert>,
+    );
+
+    expect(queryByTestId(alertTestId)).toBeNull();
+  });
+
+  test("should call onClose callback when clicking the alert", () => {
+    const onCloseMock = jest.fn();
+    const { getByTestId } = render(
+      <Alert variant="success" onClose={onCloseMock} isVisible={true} testId={alertTestId}>
+        Success Alert
+      </Alert>,
+    );
+
+    fireEvent.click(getByTestId(alertTestId));
+
+    expect(onCloseMock).toHaveBeenCalled();
   });
 });

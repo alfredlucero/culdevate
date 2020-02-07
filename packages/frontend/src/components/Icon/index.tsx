f46df9f27865d5a0d559187edfa7a8406e63090f
alfredlucero <alfred.lucero@ucla.edu>
2020-02-07 13:54:08 -0800

    [#77] Installed React FontAwesome icons and created Icon component

---
+++ packages/frontend/src/components/Icon/index.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import cn from "classnames";
+import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSuitcase,
+  faSchool,
+  faAward,
+  faBook,
+  faCertificate,
+  faProjectDiagram,
+  faUsers,
+  faAddressCard,
+  faHighlighter,
+  faChevronCircleRight,
+  faTimes,
+  faTrashAlt,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { CommonProps } from "../commonProps";
+
+export interface IconProps extends CommonProps, Omit<FontAwesomeIconProps, "icon" | "size"> {
+  variant: IconVariant;
+  size: IconSize;
+  onClick?: (e: React.MouseEvent) => void;
+}
+
+export type IconVariant =
+  | "work"
+  | "education"
+  | "accomplishments"
+  | "publications"
+  | "skills"
+  | "projects"
+  | "organizations"
+  | "references"
+  | "other"
+  | "bulletpoint"
+  | "x"
+  | "trash"
+  | "plus";
+
+export type IconSize = "small" | "medium" | "large";
+
+const iconVariantToFontAwesomeMap: IconVariantToFontAwesomeMap = {
+  work: faSuitcase,
+  education: faSchool,
+  accomplishments: faAward,
+  publications: faBook,
+  skills: faCertificate,
+  projects: faProjectDiagram,
+  organizations: faUsers,
+  references: faAddressCard,
+  other: faHighlighter,
+  bulletpoint: faChevronCircleRight,
+  x: faTimes,
+  trash: faTrashAlt,
+  plus: faPlus,
+};
+
+type IconVariantToFontAwesomeMap = {
+  [iconVariant in IconVariant]: IconDefinition;
+};
+
+const Icon: React.FC<IconProps> = ({
+  variant,
+  size,
+  onClick = () => {},
+  testId = "",
+  className = "",
+  ...restOfIconProps
+}) => {
+  const icon = iconVariantToFontAwesomeMap[variant];
+  return (
+    <div
+      onClick={onClick}
+      className={cn(
+        "font-sans",
+        "text-gray-800",
+        {
+          "text-xl": size === "small",
+          "text-2xl": size === "medium",
+          "text-3xl": size === "large",
+        },
+        className,
+      )}
+      {...(testId !== "" ? { "data-testid": testId } : {})}
+    >
+      <FontAwesomeIcon icon={icon} {...restOfIconProps} />
+    </div>
+  );
+};
+
+export default Icon;
---
+++ packages/frontend/src/pages/Recaps/components/BulletPoint/index.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+describe("<BulletPoint />", () => {
+  test("should be true", () => {
+    expect(true).toBe(true);
+  });
+});

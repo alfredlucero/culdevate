6727dd45e6c8da230acda439bdbb8ee5178381a3
alfredlucero <alfred.lucero@ucla.edu>
2020-02-07 19:08:58 -0800

    [#77] Implemented BulletPoint component

--- packages/frontend/src/pages/Recaps/components/BulletPoint/index.test.tsx
+++ packages/frontend/src/pages/Recaps/components/BulletPoint/index.test.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import BulletPoint from "./index";
 
 describe("<BulletPoint />", () => {
-  test("should be true", () => {
-    expect(true).toBe(true);
+  const bulletPointTestId = "bulletPointTestId";
+
+  test("should render without error", () => {
+    const { container } = render(
+      <BulletPoint bulletPoint="Some bullet point!" testId={bulletPointTestId} className="extra-bulletpoint-class" />,
+    );
+
+    expect(container).toMatchSnapshot();
   });
 });

50b1be144e362bcc82e9fc4170a169b208965a04
Alfred Lucero <alfredlucero@users.noreply.github.com>
2020-02-17 21:55:31 -0800

    [#90] Implemented SkillsRecap component (#111)

---
+++ packages/frontend/src/pages/Recaps/components/Skills/Recap/index.test.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { RecapSkills } from "../../../../../interfaces/recaps.interface";
+import SkillsRecap from "./index";
+
+const skills: RecapSkills = {
+  title: "Tagalog",
+  kind: "Skills",
+  userId: "userId",
+  _id: "skillsId",
+  proficiency: "Intermediate",
+  bulletPoints: [
+    "Not a fluent speaker but can understand every day Tagalog speaking well",
+    "Can speak some Tagalog here and there but struggle with grammar",
+  ],
+};
+
+describe("<SkillsRecap />", () => {
+  const testId = "skillsRecapTestId";
+
+  test("should render without error", () => {
+    const { container } = render(
+      <SkillsRecap
+        skills={skills}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        testId={testId}
+        className="extra-recap-class"
+      />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});

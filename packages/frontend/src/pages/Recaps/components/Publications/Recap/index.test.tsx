7ab5cb324f264a9cddf7e1b6ba95d1fe97323198
alfredlucero <alfred.lucero@ucla.edu>
2020-02-17 21:42:33 -0800

    [#89] Implemented PublicationsRecap component

---
+++ packages/frontend/src/pages/Recaps/components/Publications/Recap/index.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { RecapPublications } from "../../../../../interfaces/recaps.interface";
+import PublicationsRecap from "./index";
+
+const publications: RecapPublications = {
+  title: "Mindfulness-based Interventions for those with PTSD",
+  kind: "Publications",
+  coauthors: "Gingin D.",
+  userId: "userId",
+  _id: "publicationsId",
+  type: "Journal",
+  bulletPoints: ["Providing meditation and cognitive behavioral therapy techniques for those with PTSD"],
+  publisher: "UCI Psychology",
+  startDate: new Date("2020/10/20"),
+  url: "http://psychology.journal.com",
+};
+
+describe("<PublicationsRecap />", () => {
+  const testId = "publicationsRecapTestId";
+
+  test("should render without error", () => {
+    const { container } = render(
+      <PublicationsRecap
+        publications={publications}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        testId={testId}
+        className="extra-recap-class"
+      />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});

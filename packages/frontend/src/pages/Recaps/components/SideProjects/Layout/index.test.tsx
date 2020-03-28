5c38e5237473ac13a7e1c7c0da3f1699a3337890
alfredlucero <alfred.lucero@ucla.edu>
2020-03-28 15:57:20 -0700

    [#114] Integrated delete flow to side projects layout

--- packages/frontend/src/pages/Recaps/components/SideProjects/Layout/index.test.tsx
+++ packages/frontend/src/pages/Recaps/components/SideProjects/Layout/index.test.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitForElementToBeRemoved } from "@testing-library/react";
 import SideProjectsLayout, { SideProjectsLayoutProps } from "./index";
+import { RecapSideProjects } from "../../../recaps.interface";
+import * as RecapsService from "../../../recaps.service";
 
+const sampleRecapSideProjects: RecapSideProjects = {
+  title: "Zeta Mu Beta Website",
+  creators: "Alfred Lucero and Regine Deguzman",
+  startDate: new Date("2016/11/01").toISOString(),
+  endDate: new Date("2016/12/31").toISOString(),
+  kind: "Side Projects",
+  userId: "userId",
+  _id: "sideProjectsId",
+  bulletPoints: ["Created fraternity website on www.zetamubeta.org with MEAN stack"],
+};
 const defaultProps: SideProjectsLayoutProps = {
-  recaps: [
-    {
-      title: "Zeta Mu Beta Website",
-      creators: "Alfred Lucero and Regine Deguzman",
-      startDate: new Date("2016/11/01").toISOString(),
-      endDate: new Date("2016/12/31").toISOString(),
-      kind: "Side Projects",
-      userId: "userId",
-      _id: "sideProjectsId",
-      bulletPoints: ["Created fraternity website on www.zetamubeta.org with MEAN stack"],
-    },
-  ],
+  recaps: [sampleRecapSideProjects],
   onGoBackToLanding: () => {},
   onCreateRecapSuccess: () => {},
   onUpdateRecapSuccess: () => {},
@@ -48,5 +49,49 @@ describe("<SideProjectsLayout />", () => {
     expect(queryByTestId("sideProjectsEmptyCard")).toBeNull();
 
     expect(getByTestId("sideProjectsRecap")).toBeVisible();
+  });
+
+  test("should show the success alert after deleting a recap successfully", async () => {
+    const onDeleteRecapSuccessMock = jest.fn();
+    const { getByText, queryByText, findByText } = render(
+      <SideProjectsLayout {...defaultProps} onDeleteRecapSuccess={onDeleteRecapSuccessMock} />,
+    );
+
+    fireEvent.click(getByText("Delete"));
+
+    expect(getByText("Are you sure you want to delete this?")).toBeVisible();
+
+    jest.spyOn(RecapsService, "deleteRecap").mockImplementationOnce(() => Promise.resolve(sampleRecapSideProjects));
+
+    fireEvent.click(getByText("Confirm Delete"));
+
+    await waitForElementToBeRemoved(() => queryByText("Are you sure you want to delete this?"));
+
+    expect(onDeleteRecapSuccessMock).toHaveBeenCalled();
+    const successAlert = await findByText("You have successfully deleted the Side Projects Recap!");
+    expect(successAlert).toBeVisible();
+  });
+
+  test("should show the error alert after failing to delete a recap", async () => {
+    const onDeleteRecapSuccessMock = jest.fn();
+    const { getByText, queryByText, findByText } = render(
+      <SideProjectsLayout {...defaultProps} onDeleteRecapSuccess={onDeleteRecapSuccessMock} />,
+    );
+
+    fireEvent.click(getByText("Delete"));
+
+    expect(getByText("Are you sure you want to delete this?")).toBeVisible();
+
+    jest.spyOn(RecapsService, "deleteRecap").mockImplementationOnce(() => Promise.reject({}));
+
+    fireEvent.click(getByText("Confirm Delete"));
+
+    await waitForElementToBeRemoved(() => queryByText("Are you sure you want to delete this?"));
+
+    expect(onDeleteRecapSuccessMock).not.toHaveBeenCalled();
+    const errorAlert = await findByText("Something went wrong with deleting the Side Projects Recap!", {
+      exact: false,
+    });
+    expect(errorAlert).toBeVisible();
   });
 });

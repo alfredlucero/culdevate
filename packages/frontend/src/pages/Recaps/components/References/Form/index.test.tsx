bd56209db7febade93d5036fa3aae0a5e6753d43
Alfred Lucero <alfredlucero@users.noreply.github.com>
2020-05-13 09:53:35 -0700

    [#99] Implemented ReferencesForm component (#153)

--- packages/frontend/src/pages/Recaps/components/References/Form/index.test.tsx
+++ packages/frontend/src/pages/Recaps/components/References/Form/index.test.tsx
@@ -1,8 +1,259 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import ReferencesForm, { ReferencesFormProps } from "./index";
+import { RecapReferences, RecapKind } from "../../../recaps.interface";
+import { MAX_GENERAL_LENGTH, recapReferencesErrors as validationErrors } from "../../../recaps.schema";
+import * as RecapsService from "../../../recaps.service";
+import { RecapFields } from "../../../recaps.schema";
+
+const initialRecap: RecapReferences = {
+  title: "Andrew C. - Product Manager",
+  kind: RecapKind.References,
+  userId: "userId",
+  _id: "referencesId",
+  company: "Sandia National Laboratories",
+  bulletPoints: [],
+  phoneNumber: "555-555-5555",
+  email: "ac@sandia.test",
+};
+const defaultProps: ReferencesFormProps = {
+  initialRecap,
+  onSaveSuccess: () => {},
+  isShowing: true,
+  onHide: () => {},
+};
 
 describe("<ReferencesForm />", () => {
   test("should render without error", () => {
-    expect(true).toBe(true);
+    const { container } = render(
+      <ReferencesForm {...defaultProps} testId="referencesFormTestId" className="extra-class-name" />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should render pre-populated form inputs given initial recap", () => {
+    const { container } = render(
+      <ReferencesForm {...defaultProps} testId="referencesFormTestId" className="extra-class-name" />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should show empty form inputs and disabled state given no initial recap", () => {
+    const { container } = render(
+      <ReferencesForm
+        {...defaultProps}
+        initialRecap={null}
+        testId="referencesFormTestId"
+        className="extra-class-name"
+      />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should show title required error after blurring with no title", async () => {
+    const { getByLabelText, findByText, getByTestId } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesTitle), { target: { value: "" } });
+    fireEvent.blur(getByLabelText(RecapFields.referencesTitle));
+
+    const requiredError = await findByText(validationErrors.titleRequired);
+
+    expect(requiredError).toBeVisible();
+    expect(getByTestId("referencesSaveButton")).toBeDisabled();
+  });
+
+  test("should show title max length error after blurring with title beyond max length", async () => {
+    const { getByLabelText, findByText, getByTestId } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesTitle), {
+      target: { value: "a".repeat(MAX_GENERAL_LENGTH + 1) },
+    });
+    fireEvent.blur(getByLabelText(RecapFields.referencesTitle));
+
+    const maxLengthError = await findByText(validationErrors.titleMaxLength);
+
+    expect(maxLengthError).toBeVisible();
+    expect(getByTestId("referencesSaveButton")).toBeDisabled();
+  });
+
+  test("should show title without inline error after blurring with valid title", async () => {
+    const { getByLabelText, findByText, queryByText } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesTitle), { target: { value: "" } });
+    fireEvent.blur(getByLabelText(RecapFields.referencesTitle));
+
+    const requiredError = await findByText(validationErrors.titleRequired);
+    expect(requiredError).toBeVisible();
+
+    fireEvent.change(getByLabelText(RecapFields.referencesTitle), { target: { value: "title" } });
+    fireEvent.blur(getByLabelText(RecapFields.referencesTitle));
+
+    expect(queryByText(validationErrors.titleRequired)).toBeNull();
+  });
+
+  test("should show company required error after blurring with no company", async () => {
+    const { getByLabelText, findByText, getByTestId } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesCompany), { target: { value: "" } });
+    fireEvent.blur(getByLabelText(RecapFields.referencesCompany));
+
+    const requiredError = await findByText(validationErrors.companyRequired);
+
+    expect(requiredError).toBeVisible();
+    expect(getByTestId("referencesSaveButton")).toBeDisabled();
+  });
+
+  test("should show company max length error after blurring with company beyond max length", async () => {
+    const { getByLabelText, findByText, getByTestId } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesCompany), {
+      target: { value: "a".repeat(MAX_GENERAL_LENGTH + 1) },
+    });
+    fireEvent.blur(getByLabelText(RecapFields.referencesCompany));
+
+    const maxLengthError = await findByText(validationErrors.companyMaxLength);
+
+    expect(maxLengthError).toBeVisible();
+    expect(getByTestId("referencesSaveButton")).toBeDisabled();
+  });
+
+  test("should show company without inline error after blurring with valid company", async () => {
+    const { getByLabelText, findByText, queryByText } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesCompany), { target: { value: "" } });
+    fireEvent.blur(getByLabelText(RecapFields.referencesCompany));
+
+    const requiredError = await findByText(validationErrors.companyRequired);
+    expect(requiredError).toBeVisible();
+
+    fireEvent.change(getByLabelText(RecapFields.referencesCompany), { target: { value: "company" } });
+    fireEvent.blur(getByLabelText(RecapFields.referencesCompany));
+
+    expect(queryByText(validationErrors.companyRequired)).toBeNull();
+  });
+
+  test("should show email max length error after blurring with email beyond max length", async () => {
+    const { getByLabelText, findByText, getByTestId } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesEmail), {
+      target: { value: "email@test.com" + "a".repeat(MAX_GENERAL_LENGTH) },
+    });
+    fireEvent.blur(getByLabelText(RecapFields.referencesEmail));
+
+    const maxLengthError = await findByText(validationErrors.emailMaxLength);
+
+    expect(maxLengthError).toBeVisible();
+    expect(getByTestId("referencesSaveButton")).toBeDisabled();
+  });
+
+  test("should show email invalid syntax error after blurring with invalid email", async () => {
+    const { getByLabelText, findByText, getByTestId } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesEmail), {
+      target: { value: "invalidemail" },
+    });
+    fireEvent.blur(getByLabelText(RecapFields.referencesEmail));
+
+    const invalidEmailError = await findByText(validationErrors.emailInvalid);
+
+    expect(invalidEmailError).toBeVisible();
+    expect(getByTestId("referencesSaveButton")).toBeDisabled();
+  });
+
+  test("should show email without inline error given valid/empty email", async () => {
+    const { getByLabelText, queryByText } = render(<ReferencesForm {...defaultProps} initialRecap={null} />);
+
+    expect(queryByText(validationErrors.emailInvalid)).toBeNull();
+
+    fireEvent.change(getByLabelText(RecapFields.referencesEmail), {
+      target: { value: "validemail@test.com" },
+    });
+    fireEvent.blur(getByLabelText(RecapFields.referencesEmail));
+
+    expect(queryByText(validationErrors.emailInvalid)).toBeNull();
+  });
+
+  test("should show phone number max length error after blurring with phone number beyond max length", async () => {
+    const { getByLabelText, findByText, getByTestId } = render(
+      <ReferencesForm {...defaultProps} initialRecap={null} />,
+    );
+
+    fireEvent.change(getByLabelText(RecapFields.referencesPhoneNumber), {
+      target: { value: "a".repeat(MAX_GENERAL_LENGTH + 1) },
+    });
+    fireEvent.blur(getByLabelText(RecapFields.referencesPhoneNumber));
+
+    const maxLengthError = await findByText(validationErrors.phoneNumberMaxLength);
+
+    expect(maxLengthError).toBeVisible();
+    expect(getByTestId("referencesSaveButton")).toBeDisabled();
+  });
+
+  test("should show phone number without inline error given valid/empty phone number", async () => {
+    const { getByLabelText, queryByText } = render(<ReferencesForm {...defaultProps} initialRecap={null} />);
+
+    expect(queryByText(validationErrors.phoneNumberMaxLength)).toBeNull();
+
+    fireEvent.change(getByLabelText(RecapFields.referencesPhoneNumber), {
+      target: { value: "555-555-5555" },
+    });
+    fireEvent.blur(getByLabelText(RecapFields.referencesPhoneNumber));
+
+    expect(queryByText(validationErrors.phoneNumberMaxLength)).toBeNull();
+  });
+
+  test("should call onHide after clicking Cancel button", () => {
+    const onHideMock = jest.fn();
+    const { getByText } = render(<ReferencesForm {...defaultProps} initialRecap={null} onHide={onHideMock} />);
+
+    fireEvent.click(getByText("Cancel"));
+
+    expect(onHideMock).toHaveBeenCalled();
+  });
+
+  test("should call onSaveSuccess after saving successfully with valid fields", async () => {
+    const onSaveSuccessMock = jest.fn();
+    const { getByTestId } = render(<ReferencesForm {...defaultProps} onSaveSuccess={onSaveSuccessMock} />);
+
+    jest.spyOn(RecapsService, "updateRecap").mockImplementationOnce(() => Promise.resolve(initialRecap));
+
+    expect(getByTestId("referencesSaveButton")).not.toBeDisabled();
+    fireEvent.click(getByTestId("referencesSaveButton"));
+
+    await waitFor(() => {
+      expect(onSaveSuccessMock).toHaveBeenCalled();
+    });
+  });
+
+  test("should not call onSaveSuccess after failing to save successfully with valid fields", async () => {
+    const onSaveSuccessMock = jest.fn();
+    const { getByTestId, findByText } = render(<ReferencesForm {...defaultProps} onSaveSuccess={onSaveSuccessMock} />);
+
+    jest.spyOn(RecapsService, "updateRecap").mockImplementationOnce(() => Promise.reject({}));
+
+    expect(getByTestId("referencesSaveButton")).not.toBeDisabled();
+    fireEvent.click(getByTestId("referencesSaveButton"));
+
+    await findByText("Something went wrong with updating a References Recap!", { exact: false });
+    expect(onSaveSuccessMock).not.toHaveBeenCalled();
   });
 });
--- packages/frontend/src/pages/Recaps/components/References/Form/index.tsx
+++ packages/frontend/src/pages/Recaps/components/References/Form/index.tsx
@@ -1,7 +1,342 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import cn from "classnames";
+import { reach } from "yup";
+import TextInput from "../../../../../components/TextInput";
+import BulletPointInputList from "../../BulletPointInputList";
+import Button from "../../../../../components/Button";
+import { RecapsCreateErrorAlert, RecapsUpdateErrorAlert } from "../../RecapsAlerts";
+import { CommonProps } from "../../../../../components/commonProps";
+import { RecapReferences, RecapCreate, RecapKind } from "../../../recaps.interface";
+import { useBulletPointInputList } from "../../../hooks/useBulletPointInputList";
+import { createRecap, updateRecap } from "../../../recaps.service";
+import { RecapReferencesSchema, RecapFields } from "../../../recaps.schema";
 
-const ReferencesForm = () => {
-  return <div>References Form</div>;
+export interface ReferencesFormProps extends CommonProps {
+  initialRecap: RecapReferences | null;
+  onSaveSuccess: (savedRecap: RecapReferences) => void;
+  isShowing: boolean;
+  onHide: () => void;
+}
+
+const ReferencesForm: React.FC<ReferencesFormProps> = ({
+  initialRecap,
+  onSaveSuccess,
+  isShowing,
+  onHide,
+  testId = "",
+  className = "",
+  ...passThroughProps
+}) => {
+  const [title, setTitle] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
+  const onBlurTitle = (e: React.FocusEvent<HTMLInputElement>) => {
+    const currentTitle = e.currentTarget.value;
+    try {
+      reach(RecapReferencesSchema, "title").validateSync(currentTitle);
+      setTitleError("");
+    } catch (err) {
+      setTitleError(err.message);
+    }
+  };
+
+  const [company, setCompany] = useState("");
+  const [companyError, setCompanyError] = useState("");
+  const onChangeCompany = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCompany(e.currentTarget.value);
+  };
+  const onBlurCompany = (e: React.FocusEvent<HTMLInputElement>) => {
+    const currentCompany = e.currentTarget.value;
+    try {
+      reach(RecapReferencesSchema, "company").validateSync(currentCompany);
+      setCompanyError("");
+    } catch (err) {
+      setCompanyError(err.message);
+    }
+  };
+
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+  const onBlurEmail = (e: React.FocusEvent<HTMLInputElement>) => {
+    const currentEmail = e.currentTarget.value;
+    try {
+      reach(RecapReferencesSchema, "email").validateSync(currentEmail);
+      setEmailError("");
+    } catch (err) {
+      setEmailError(err.message);
+    }
+  };
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [phoneNumberError, setPhoneNumberError] = useState("");
+  const onChangePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(e.currentTarget.value);
+  };
+  const onBlurPhoneNumber = (e: React.FocusEvent<HTMLInputElement>) => {
+    const currentPhoneNumber = e.currentTarget.value;
+    try {
+      reach(RecapReferencesSchema, "phoneNumber").validateSync(currentPhoneNumber);
+      setTitleError("");
+    } catch (err) {
+      setTitleError(err.message);
+    }
+  };
+
+  const {
+    bulletPointInputList,
+    onDragEnd,
+    onChangeBulletPointInput,
+    onBlurBulletPointInput,
+    onAddBulletPointInput,
+    onDeleteBulletPointInput,
+    isAddBulletPointInputDisabled,
+    resetBulletPointInputList,
+  } = useBulletPointInputList({
+    initialBulletPoints: [],
+    idPrefix: "referencesBulletPoint",
+  });
+
+  const clearOutInputs = () => {
+    setTitle("");
+    setTitleError("");
+    setCompany("");
+    setCompanyError("");
+    setPhoneNumber("");
+    setPhoneNumberError("");
+    setEmail("");
+    setEmailError("");
+    resetBulletPointInputList([]);
+  };
+
+  useEffect(() => {
+    // Upon closing out the modal/swapping forms, clear out all the inputs to their default empty values
+    if (!isShowing) {
+      clearOutInputs();
+      return;
+    }
+
+    // Upon showing the form/modal to edit a recap, set the inputs to match the recap's initial values
+    if (initialRecap !== null) {
+      setTitle(initialRecap.title);
+      setTitleError("");
+      setCompany(initialRecap.company);
+      setCompanyError("");
+      setEmail(initialRecap.email);
+      setEmailError("");
+      setPhoneNumber(initialRecap.phoneNumber);
+      setPhoneNumberError("");
+      resetBulletPointInputList(initialRecap.bulletPoints);
+    } else {
+      // Otherwise, for creating a modal, clear out all the inputs to their default empty values
+      clearOutInputs();
+    }
+  }, [initialRecap, isShowing]);
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitCreateError, setIsSubmitCreateError] = useState(false);
+  const [isSubmitUpdateError, setIsSubmitUpdateError] = useState(false);
+  const onHideSubmitCreateError = () => {
+    setIsSubmitCreateError(false);
+  };
+  const onHideSubmitUpdateError = () => {
+    setIsSubmitUpdateError(false);
+  };
+
+  const triggerUpdateRecap = () => {
+    if (!initialRecap) {
+      return;
+    }
+
+    const { _id, userId, kind } = initialRecap;
+    const updatedBulletPoints = bulletPointInputList
+      .filter(bulletPoint => bulletPoint.value !== "")
+      .map(bulletPoint => bulletPoint.value);
+    const updatedReferencesRecap: RecapReferences = {
+      _id,
+      userId,
+      kind,
+      bulletPoints: updatedBulletPoints,
+      title,
+      company,
+      phoneNumber,
+      email,
+    };
+
+    setIsSubmitting(true);
+    updateRecap(updatedReferencesRecap)
+      .then(savedRecap => {
+        clearOutInputs();
+        setIsSubmitting(false);
+        setIsSubmitUpdateError(false);
+        onSaveSuccess(savedRecap as RecapReferences);
+        onHide();
+      })
+      .catch(() => {
+        setIsSubmitting(false);
+        setIsSubmitUpdateError(true);
+      });
+  };
+
+  const triggerCreateRecap = () => {
+    const updatedBulletPoints = bulletPointInputList
+      .filter(bulletPoint => bulletPoint.value !== "")
+      .map(bulletPoint => bulletPoint.value);
+    const recapToCreate: RecapCreate = {
+      kind: RecapKind.References,
+      bulletPoints: updatedBulletPoints,
+      title,
+      company,
+      email,
+      phoneNumber,
+    };
+
+    setIsSubmitting(true);
+    createRecap(recapToCreate)
+      .then(savedRecap => {
+        clearOutInputs();
+        setIsSubmitting(false);
+        setIsSubmitCreateError(false);
+        onSaveSuccess(savedRecap as RecapReferences);
+        onHide();
+      })
+      .catch(() => {
+        setIsSubmitting(false);
+        setIsSubmitCreateError(true);
+      });
+  };
+
+  const hasValidInputs =
+    title &&
+    !titleError &&
+    company &&
+    !companyError &&
+    !emailError &&
+    !phoneNumberError &&
+    bulletPointInputList.every(bulletPointInput => bulletPointInput.valid);
+  const isSaveButtonDisabled = isSubmitting || !hasValidInputs;
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (isSaveButtonDisabled) {
+      return;
+    }
+
+    const isEditingRecap = initialRecap !== null;
+    if (isEditingRecap) {
+      triggerUpdateRecap();
+    } else {
+      triggerCreateRecap();
+    }
+  };
+
+  return (
+    <div className={cn("mt-4", className)} {...(testId !== "" ? { "data-testid": testId } : {})} {...passThroughProps}>
+      <RecapsCreateErrorAlert
+        kind={RecapKind.References}
+        isShowing={isSubmitCreateError}
+        onHide={onHideSubmitCreateError}
+      />
+      <RecapsUpdateErrorAlert
+        kind={RecapKind.References}
+        isShowing={isSubmitUpdateError}
+        onHide={onHideSubmitUpdateError}
+      />
+      <form onSubmit={onSubmit}>
+        <div className="w-full mb-4">
+          <TextInput
+            id="title"
+            placeholder="What is the name or title of someone you worked with?"
+            value={title}
+            onChange={onChangeTitle}
+            onBlur={onBlurTitle}
+            type="text"
+            required={true}
+            label={RecapFields.referencesTitle}
+            valid={!titleError}
+            errorInfo={titleError}
+            className="mr-4"
+          />
+        </div>
+
+        <div className="w-full mb-4">
+          <TextInput
+            id="company"
+            placeholder="Which company did you both work together at?"
+            value={company}
+            onChange={onChangeCompany}
+            onBlur={onBlurCompany}
+            type="text"
+            required={true}
+            label={RecapFields.referencesCompany}
+            valid={!companyError}
+            errorInfo={companyError}
+            className="mr-4"
+          />
+        </div>
+
+        <div className="flex w-full mb-4">
+          <TextInput
+            id="email"
+            placeholder="What is your reference's email?"
+            value={email}
+            onChange={onChangeEmail}
+            onBlur={onBlurEmail}
+            type="text"
+            required={true}
+            label={RecapFields.referencesEmail}
+            valid={!emailError}
+            errorInfo={emailError}
+            className="w-1/2 mr-4"
+          />
+          <TextInput
+            id="phoneNumber"
+            placeholder="What was your reference's phone number?"
+            value={phoneNumber}
+            onChange={onChangePhoneNumber}
+            onBlur={onBlurPhoneNumber}
+            type="text"
+            required={true}
+            label={RecapFields.referencesPhoneNumber}
+            valid={!phoneNumberError}
+            errorInfo={phoneNumberError}
+            className="w-1/2 mr-4"
+          />
+        </div>
+
+        <BulletPointInputList
+          bulletPointInputList={bulletPointInputList}
+          onDragEnd={onDragEnd}
+          onChangeBulletPointInput={onChangeBulletPointInput}
+          onBlurBulletPointInput={onBlurBulletPointInput}
+          onDeleteBulletPointInput={onDeleteBulletPointInput}
+          onAddBulletPointInput={onAddBulletPointInput}
+          isAddBulletPointInputDisabled={isAddBulletPointInputDisabled}
+          className="mb-6"
+        />
+        <div className="flex justify-end">
+          <Button type="button" variant="secondary" onClick={onHide} className="mr-2">
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            variant="primary"
+            loading={isSubmitting}
+            disabled={isSaveButtonDisabled}
+            testId="referencesSaveButton"
+          >
+            {isSubmitting && <>Saving Recap...</>}
+            {!isSubmitting && <>Save Recap</>}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ReferencesForm;

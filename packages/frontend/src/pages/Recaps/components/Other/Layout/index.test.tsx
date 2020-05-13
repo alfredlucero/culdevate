4e0503129dc8837aebaa73e7468acaa91a981e6b
alfredlucero <alfred.lucero@ucla.edu>
2020-05-13 11:19:59 -0700

    [#136] Integrated Other create/update flows

--- packages/frontend/src/pages/Recaps/components/Other/Layout/index.test.tsx
+++ packages/frontend/src/pages/Recaps/components/Other/Layout/index.test.tsx
@@ -3,6 +3,7 @@ import { render, fireEvent, waitForElementToBeRemoved } from "@testing-library/r
 import OtherLayout, { OtherLayoutProps } from "./index";
 import { RecapOther, RecapKind } from "../../../recaps.interface";
 import * as RecapsService from "../../../recaps.service";
+import { RecapFields } from "../../../recaps.schema";
 
 const sampleRecapOther: RecapOther = {
   title: "Finished reading Clean Code book!",
@@ -88,5 +89,141 @@ describe("<OtherLayout />", () => {
       exact: false,
     });
     expect(errorAlert).toBeVisible();
+  });
+
+  test("should show the success alert after creating a recap successfully", async () => {
+    const onCreateRecapSuccessMock = jest.fn();
+    const { getByText, findByText, getByLabelText, getByTestId, queryByText } = render(
+      <OtherLayout {...defaultProps} onCreateRecapSuccess={onCreateRecapSuccessMock} />,
+    );
+
+    fireEvent.click(getByText("Add a Recap"));
+
+    const createModal = await findByText("Create a Other Recap");
+    expect(createModal).toBeVisible();
+
+    const recap: RecapOther = {
+      title: "Finished reading Clean Code book!",
+      kind: RecapKind.Other,
+      userId: "userId",
+      _id: "otherId",
+      startDate: new Date("2017/10/20").toISOString(),
+      endDate: new Date("2017/12/20").toISOString(),
+      bulletPoints: ["Learned about how to write cleaner and more maintainable code", "Improved with code reviews"],
+    };
+
+    fireEvent.change(getByLabelText(RecapFields.otherTitle), { target: { value: recap.title } });
+    fireEvent.blur(getByLabelText(RecapFields.otherTitle));
+
+    fireEvent.change(getByLabelText(RecapFields.startDate), { target: { value: "01/2020" } });
+    fireEvent.blur(getByLabelText(RecapFields.startDate));
+
+    fireEvent.change(getByLabelText(RecapFields.endDate), { target: { value: "10/2020" } });
+    fireEvent.blur(getByLabelText(RecapFields.endDate));
+
+    expect(getByTestId("otherSaveButton")).not.toBeDisabled();
+
+    jest.spyOn(RecapsService, "createRecap").mockImplementationOnce(() => Promise.resolve(recap));
+
+    fireEvent.click(getByTestId("otherSaveButton"));
+
+    await waitForElementToBeRemoved(() => queryByText("Create a Other Recap"));
+
+    expect(onCreateRecapSuccessMock).toHaveBeenCalled();
+    expect(queryByText("You have successfully created a Other Recap!")).toBeVisible();
+  });
+
+  test("should show the error alert after failing to create a recap", async () => {
+    const onCreateRecapSuccessMock = jest.fn();
+    const { getByText, findByText, getByLabelText, getByTestId } = render(
+      <OtherLayout {...defaultProps} onCreateRecapSuccess={onCreateRecapSuccessMock} />,
+    );
+
+    fireEvent.click(getByText("Add a Recap"));
+
+    const createModal = await findByText("Create a Other Recap");
+    expect(createModal).toBeVisible();
+
+    const recap: RecapOther = {
+      title: "Finished reading Clean Code book!",
+      kind: RecapKind.Other,
+      userId: "userId",
+      _id: "otherId",
+      startDate: new Date("2017/10/20").toISOString(),
+      endDate: new Date("2017/12/20").toISOString(),
+      bulletPoints: ["Learned about how to write cleaner and more maintainable code", "Improved with code reviews"],
+    };
+
+    fireEvent.change(getByLabelText(RecapFields.otherTitle), { target: { value: recap.title } });
+    fireEvent.blur(getByLabelText(RecapFields.otherTitle));
+
+    fireEvent.change(getByLabelText(RecapFields.startDate), { target: { value: "01/2020" } });
+    fireEvent.blur(getByLabelText(RecapFields.startDate));
+
+    fireEvent.change(getByLabelText(RecapFields.endDate), { target: { value: "10/2020" } });
+    fireEvent.blur(getByLabelText(RecapFields.endDate));
+
+    expect(getByTestId("otherSaveButton")).not.toBeDisabled();
+
+    jest.spyOn(RecapsService, "createRecap").mockImplementationOnce(() => Promise.reject({}));
+
+    fireEvent.click(getByTestId("otherSaveButton"));
+
+    const createErrorAlert = await findByText("Something went wrong with adding a Other Recap!", {
+      exact: false,
+    });
+
+    expect(createErrorAlert).toBeVisible();
+    expect(getByText("Create a Other Recap")).toBeVisible();
+    expect(onCreateRecapSuccessMock).not.toHaveBeenCalled();
+  });
+
+  test("should show the success alert after updating a recap successfully", async () => {
+    const onUpdateRecapSuccessMock = jest.fn();
+    const { getByText, queryByText, findByText, getByTestId } = render(
+      <OtherLayout {...defaultProps} onUpdateRecapSuccess={onUpdateRecapSuccessMock} />,
+    );
+
+    fireEvent.click(getByText("Edit"));
+
+    const editModal = await findByText("Edit your Other Recap");
+    expect(editModal).toBeVisible();
+
+    expect(getByTestId("otherSaveButton")).not.toBeDisabled();
+
+    jest.spyOn(RecapsService, "updateRecap").mockImplementationOnce(() => Promise.resolve(sampleRecapOther));
+
+    fireEvent.click(getByTestId("otherSaveButton"));
+
+    await waitForElementToBeRemoved(() => queryByText("Edit your Other Recap"));
+
+    expect(queryByText("You have successfully updated a Other Recap!")).toBeVisible();
+    expect(onUpdateRecapSuccessMock).toHaveBeenCalled();
+  });
+
+  test("should show the error alert after failing to update a recap", async () => {
+    const onUpdateRecapSuccessMock = jest.fn();
+    const { getByText, queryByText, findByText, getByTestId } = render(
+      <OtherLayout {...defaultProps} onUpdateRecapSuccess={onUpdateRecapSuccessMock} />,
+    );
+
+    fireEvent.click(getByText("Edit"));
+
+    const editModal = await findByText("Edit your Other Recap");
+    expect(editModal).toBeVisible();
+
+    expect(getByTestId("otherSaveButton")).not.toBeDisabled();
+
+    jest.spyOn(RecapsService, "updateRecap").mockImplementationOnce(() => Promise.reject({}));
+
+    fireEvent.click(getByTestId("otherSaveButton"));
+
+    const updateErrorAlert = await findByText("Something went wrong with updating a Other Recap!", {
+      exact: false,
+    });
+    expect(updateErrorAlert).toBeVisible();
+
+    expect(queryByText("Edit your Other Recap")).toBeVisible();
+    expect(onUpdateRecapSuccessMock).not.toHaveBeenCalled();
   });
 });

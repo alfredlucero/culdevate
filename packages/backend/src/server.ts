d8804e9c4b375b83f9b26069bd6f8e5fed662d7e
alfredlucero <alfred.lucero@ucla.edu>
2020-06-15 23:17:48 -0700

    Updating commands, linters, etc.

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,19 +1,19 @@
-import dotenv from "dotenv";
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({
+    path: `./.env.${process.env.CONFIG_ENV}`,
+  });
+}
 import app from "./app";
 import { setupDatabase } from "./db";
 import { validateEnv } from "./utils/validateEnv";
 
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({
-    path: `./.env.${process.env.CONFIG_ENV}`,
-  });
-}
-
 // We separate the set up of the Express server (app) from actually listening/starting up the server
 // for testing purposes and for greater separation of concerns
 
-// Validate the .env file has environment variables properly filled out; otherwise, error out
-validateEnv();
+if (process.env.NODE_ENV !== "production") {
+  // Validate the .env file has environment variables properly filled out; otherwise, error out
+  validateEnv();
+}
 
 // Start up the backend server against the environment's PORT
 app.listen(process.env.PORT, () => {

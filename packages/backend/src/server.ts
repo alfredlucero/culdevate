ffc3ebdbdfd91d423acd270bec8cad6f84fb5526
alfredlucero <alfred.lucero@ucla.edu>
2020-06-15 23:55:37 -0700

    Moving server.ts comments

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -11,12 +11,11 @@ if (process.env.NODE_ENV !== "production") {
 import app from "./app";
 import { setupDatabase } from "./db";
 
-// We separate the set up of the Express server (app) from actually listening/starting up the server
-// for testing purposes and for greater separation of concerns
-
 // Start up the backend server against the environment's PORT
 app.listen(process.env.PORT, () => {
   console.log(`Culdevate API starting on port ${process.env.PORT}...`);
 });
 
+// We separate the set up of the Express server (app) from actually listening/starting up the server
+// for testing purposes and for greater separation of concerns
 setupDatabase();

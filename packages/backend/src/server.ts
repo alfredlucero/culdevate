c3b767a226d8d4f93ba586a103906beaa8dc9713
alfredlucero <alfred.lucero@ucla.edu>
2020-06-15 23:25:45 -0700

    Fixing broken production envalid reference

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -2,18 +2,14 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({
     path: `./.env.${process.env.CONFIG_ENV}`,
   });
+  // Validate the .env file has environment variables properly filled out; otherwise, error out
+  require("./utils/validateEnv").validateEnv();
 }
 import app from "./app";
 import { setupDatabase } from "./db";
-import { validateEnv } from "./utils/validateEnv";
 
 // We separate the set up of the Express server (app) from actually listening/starting up the server
 // for testing purposes and for greater separation of concerns
-
-if (process.env.NODE_ENV !== "production") {
-  // Validate the .env file has environment variables properly filled out; otherwise, error out
-  validateEnv();
-}
 
 // Start up the backend server against the environment's PORT
 app.listen(process.env.PORT, () => {
